3b2b7be60cf563aee314e172033b7dc658e88f19
DreamEcho100 <maze6572198@gmail.com>
2022-01-31 15:37:01 +0200

    Completing the '2- Two Fer' excercise

---
+++ 2- Two Fer/two-fer.ts
@@ -0,0 +1,15 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export const twoFer = (name?: string): string => {
+	// ^                 ^                ^ this is called a return type; it's the type of the
+	// ^                 ^                  value that is returned from this function
+	// ^                 ^
+	// ^                 parameters go here
+	// ^
+	// allows the tests to import this function and call it
+	// <-- Your code goes here. You may remove all the commentary in this file.
+
+	return `One for ${name || 'you'}, one for me.`;
+};
